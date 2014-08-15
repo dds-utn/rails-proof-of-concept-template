85ae0751570c25775d9ba422d149b66ee8c7f266
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-08-15 00:14:13 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProofOfConcept::Application.config.secret_key_base = 'd27cb8e123e5dd8dfe65f3be7269aa994e36b281ea918eb4e2efd9cd05c37214cca258c75ebfff83fc9bc04cfc697e3914e0c43fa8342b8b9b4a632e0659425e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProofOfConcept::Application.config.session_store :cookie_store, key: '_proof-of-concept_session'
